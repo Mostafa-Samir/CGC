e4202c372cce95e54b27342a5187e63210d9aae0
Mostafa Samir <mostafa.3210@gmail.com>
2023-10-19 17:57:50 +0300

    add bfgs optimizer

--- cgc/optimizers.py
+++ cgc/optimizers.py
@@ -3,19 +3,21 @@ from typing import Callable
 
 import jax
 import jax.numpy as jnp
+from jax.scipy.optimize import minimize
 from tqdm import trange
+from jaxopt import LBFGS, BFGS, ScipyMinimize
 
 from cgc.early_stopper import EarlyStopper
 
 class GDOptimizer:
 
-    def __init__(self, loss_fn: Callable, learning_rate: float = 0.001, iterations_max: int = 500000, min_improvement: float = 0.01, patience: int = 1000):
+    def __init__(self, loss_fn: Callable, learning_rate: float = 0.01, iterations_max: int = 500000, min_improvement: float = 1, patience: int = 1000):
         self.learning_rate = learning_rate
         self.iterations_max = iterations_max
         self.loss_grad = jax.grad(loss_fn)
         self.jitted_loss = jax.jit(loss_fn)
         self.jitted_update_step = jax.jit(self.update_step)
-        self.early_stopper = EarlyStopper(min_improvement=min_improvement, patience=patience, improvement_type="relative")
+        self.early_stopper = EarlyStopper(min_improvement=min_improvement, patience=patience)
 
     @abstractmethod
     def update_step(self, Z, X, M):
@@ -53,4 +55,26 @@ class ProjectedNGDOptimizer(GDOptimizer):
         new_Z = Z - self.learning_rate * normed_g
         new_Z = jnp.where(M, X, new_Z)
         return new_Z
+    
+class BFGSOptimizer():
+    def __init__(self, loss_fn: Callable, learning_rate: float = 0.01, iterations_max: int = 500000, min_improvement: float = 1, patience: int = 1000):
+        self.loss_fn = loss_fn
+        self.jitted_loss = jax.jit(loss_fn)
+        self._optimizer = ScipyMinimize(method="L-BFGS-B", fun=loss_fn, jit=True, maxiter=10000)
+        #self.jitted_update_step = jax.jit(self._optimizer.update)
+
+    def run(self, Z, X, M):
+        pbar = trange(self._optimizer.maxiter)
+        def progressbar_callback(Z):
+            loss = self.jitted_loss(Z, X, M)
+            pbar.set_description(f"Loss: {loss:.4f}")
+            pbar.update(1)
+
+        self._optimizer.callback = progressbar_callback
+
+        Z, *_ = self._optimizer.run(Z, X, M)
+
+        pbar.close()
+        
+        return Z
 
